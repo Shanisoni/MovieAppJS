6757e3eefcf2651dd69ed96679d7e0155a7b2145
Shani Soni <134639145+Shanisoni@users.noreply.github.com>
2025-07-24 03:59:41 +0530

    commited

---
+++ navigation/AppNavigation.js
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen'; // Ensure you have a HomeScreen component
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Define your screens here */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
