625d2f579a00df68b5ad21549d22e7d164fde356
Shani Soni <134639145+Shanisoni@users.noreply.github.com>
2025-07-24 23:16:36 +0530

    commited

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -3,10 +3,11 @@ import React from 'react';
 import Carousel from 'react-native-snap-carousel';
 import { useNavigation } from '@react-navigation/native';
 
-var { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 export default function TrendingMovies({ data }) {
   const navigation = useNavigation();
+
   const handleClick = (item) => {
     navigation.navigate('Movie', item);
   };
@@ -24,20 +25,21 @@ export default function TrendingMovies({ data }) {
         slideStyle={{ display: 'flex', alignItems: 'center' }}
       />
     </View>
-  );
+  )
+  
 }
 
-const MovieCard = ({item , handleClick})=>{
-    return (
-        <TouchableWithoutFeedback onPress={handleClick}>
-            <Image 
-                source={require('../assets/images/moviePoster1.png')}
-                style={{
-                    width: width*0.6,
-                    height: height*0.4
-                }}
-                className="rounded-3xl"
-            />
-        </TouchableWithoutFeedback>
-    )
+const MovieCard = ({ item, handleClick }) => {
+  return (
+    <TouchableWithoutFeedback onPress={() => handleClick(item)}>
+      <Image
+        source={require('../assets/images/moviePoster1.png')}
+        style={{
+          width: width * 0.6,
+          height: height * 0.4
+        }}
+        className="rounded-3xl"
+      />
+    </TouchableWithoutFeedback>
+  )
 }
